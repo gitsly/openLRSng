cb7e31fe44583a30ccf9bdf284187ad6e3e15ad9
Martin <martin.collberg@gmail.com>
2014-09-29 11:27:45 +0200

    making some progress

--- serialport.h
+++ serialport.h
@@ -106,26 +106,137 @@ class FastSerial: public Stream {
 
 	/// Constructor
 	FastSerial(const uint8_t portNumber, volatile uint8_t *ubrrh, volatile uint8_t *ubrrl, volatile uint8_t *ucsra,
-	volatile uint8_t *ucsrb, const uint8_t u2x, const uint8_t portEnableBits, const uint8_t portTxBits);
+	volatile uint8_t *ucsrb, const uint8_t u2x, const uint8_t portEnableBits, const uint8_t portTxBits)  :
+	_ubrrh(ubrrh),
+	_ubrrl(ubrrl),
+	_ucsra(ucsra),
+	_ucsrb(ucsrb),
+	_u2x(u2x),
+	_portEnableBits(portEnableBits),
+	_portTxBits(portTxBits),
+	_rxBuffer(&__FastSerial__rxBuffer[portNumber]),
+	_txBuffer(&__FastSerial__txBuffer[portNumber])
+	{
+		setInitialized(portNumber);
+		begin(57600);
+	}
 
 	/// @name 	Serial API
 	//@{
-	virtual void begin(long baud);
-	virtual void end(void);
-	virtual int available(void);
-	virtual int txspace(void);
-	virtual int read(void);
-	virtual int peek(void);
-	virtual void flush(void);
+	virtual void begin(long baud)
+	{
+		begin(baud, 0, 0);
+	}
 
-	uint16_t rxOverflowCounter(void);
+	virtual void end(void)
+	{
+		*_ucsrb &= ~(_portEnableBits | _portTxBits);
+
+		_freeBuffer(_rxBuffer);
+		_freeBuffer(_txBuffer);
+		_open = false;
+	}
+
+	virtual int available(void)
+	{
+		if (!_open)
+		return (-1);
+		return ((_rxBuffer->head - _rxBuffer->tail) & _rxBuffer->mask);
+	}
+
+	virtual int txspace(void)
+	{
+		if (!_open)
+		return (-1);
+		return ((_txBuffer->mask+1) - ((_txBuffer->head - _txBuffer->tail) & _txBuffer->mask));
+	}
+	
+	virtual int read(void)
+	{
+		uint8_t c;
+
+		// if the head and tail are equal, the buffer is empty
+		if (!_open || (_rxBuffer->head == _rxBuffer->tail))
+		return (-1);
+
+		// pull character from tail
+		c = _rxBuffer->bytes[_rxBuffer->tail];
+		_rxBuffer->tail = (_rxBuffer->tail + 1) & _rxBuffer->mask;
+
+		return (c);
+	}
+
+	virtual int peek(void)
+	{
+
+		// if the head and tail are equal, the buffer is empty
+		if (!_open || (_rxBuffer->head == _rxBuffer->tail))
+		return (-1);
+
+		// pull character from tail
+		return (_rxBuffer->bytes[_rxBuffer->tail]);
+	}
+
+	virtual void flush(void)
+	{
+		// don't reverse this or there may be problems if the RX interrupt
+		// occurs after reading the value of _rxBuffer->head but before writing
+		// the value to _rxBuffer->tail; the previous value of head
+		// may be written to tail, making it appear as if the buffer
+		// don't reverse this or there may be problems if the RX interrupt
+		// occurs after reading the value of head but before writing
+		// the value to tail; the previous value of rx_buffer_head
+		// may be written to tail, making it appear as if the buffer
+		// were full, not empty.
+		_rxBuffer->head = _rxBuffer->tail;
+
+		// don't reverse this or there may be problems if the TX interrupt
+		// occurs after reading the value of _txBuffer->tail but before writing
+		// the value to _txBuffer->head.
+		_txBuffer->tail = _txBuffer->head;
+	}
+
+
+	uint16_t rxOverflowCounter(void)
+	{
+		if (!_open)
+		return 0;
+		return _rxBuffer->overflow;
+	}
+
 
 	#if defined(ARDUINO) && ARDUINO >= 100
-	virtual size_t write(uint8_t c);
-	#else
-	virtual void write(uint8_t c);
-	#endif
-	using BetterStream::write;
+	virtual size_t write(uint8_t c)
+	{
+		uint16_t i;
+
+		if (!_open) // drop bytes if not open
+		return 0;
+
+		// wait for room in the tx buffer
+		i = (_txBuffer->head + 1) & _txBuffer->mask;
+
+		// if the port is set into non-blocking mode, then drop the byte
+		// if there isn't enough room for it in the transmit buffer
+		if (_nonblocking_writes && i == _txBuffer->tail) {
+			return 0;
+		}
+
+		while (i == _txBuffer->tail)
+		;
+
+		// add byte to the buffer
+		_txBuffer->bytes[_txBuffer->head] = c;
+		_txBuffer->head = i;
+
+		// enable the data-ready interrupt, as it may be off if the buffer is empty
+		*_ucsrb |= _portTxBits;
+
+		// return number of bytes written (always 1)
+		return 1;
+	}
+	
+	using Stream::write;
 	//@}
 
 	/// Extended port open method
@@ -148,7 +259,62 @@ class FastSerial: public Stream {
 	///						is open, or set to ::_default_tx_buffer_size if it
 	///						is currently closed.
 	///
-	virtual void begin(long baud, unsigned int rxSpace, unsigned int txSpace);
+	virtual void begin(long baud, unsigned int rxSpace, unsigned int txSpace)
+	{
+		uint16_t ubrr;
+		bool use_u2x = true;
+
+		// if we are currently open...
+		if (_open) {
+			// If the caller wants to preserve the buffer sizing, work out what
+			// it currently is...
+			if (0 == rxSpace)
+			rxSpace = _rxBuffer->mask + 1;
+			if (0 == txSpace)
+			txSpace = _txBuffer->mask + 1;
+
+			// close the port in its current configuration, clears _open
+			end();
+		}
+
+		// allocate buffers
+		if (!_allocBuffer(_rxBuffer, rxSpace ? : _default_rx_buffer_size) || !_allocBuffer(_txBuffer, txSpace ?	: _default_tx_buffer_size)) {
+			end();
+			return; // couldn't allocate buffers - fatal
+		}
+
+		// reset buffer pointers
+		_txBuffer->head = _txBuffer->tail = 0;
+		_rxBuffer->head = _rxBuffer->tail = 0;
+
+		// mark the port as open
+		_open = true;
+
+		// If the user has supplied a new baud rate, compute the new UBRR value.
+		if (baud > 0) {
+			#if F_CPU == 16000000UL
+			// hardcoded exception for compatibility with the bootloader shipped
+			// with the Duemilanove and previous boards and the firmware on the 8U2
+			// on the Uno and Mega 2560.
+			if (baud == 57600)
+			use_u2x = false;
+			#endif
+
+			if (use_u2x) {
+				*_ucsra = 1 << _u2x;
+				ubrr = (F_CPU / 4 / baud - 1) / 2;
+				} else {
+				*_ucsra = 0;
+				ubrr = (F_CPU / 8 / baud - 1) / 2;
+			}
+
+			*_ubrrh = ubrr >> 8;
+			*_ubrrl = ubrr;
+		}
+
+		*_ucsrb |= _portEnableBits;
+	}
+
 
 	/// Transmit/receive buffer descriptor.
 	///
@@ -341,6 +507,21 @@ _BV(UDRIE##_num))
 #define FastSerialPort3(_portName)     FastSerialPort(_portName, 3)
 
 
+///////// CPP //////////
+
+#if   defined(UDR3)
+# define FS_MAX_PORTS   4
+#elif defined(UDR2)
+# define FS_MAX_PORTS   3
+#elif defined(UDR1)
+# define FS_MAX_PORTS   2
+#else
+# define FS_MAX_PORTS   1
+#endif
+
+FastSerial::Buffer __FastSerial__rxBuffer[FS_MAX_PORTS];
+FastSerial::Buffer __FastSerial__txBuffer[FS_MAX_PORTS];
+uint8_t FastSerial::_serialInitialized = 0;
 
 
 
